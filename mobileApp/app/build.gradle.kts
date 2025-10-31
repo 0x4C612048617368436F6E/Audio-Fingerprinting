5ab78eae158d30c39effe8531b05934d35e8ef2a
Benjamin <benjaminenadeba@gmail.com>
2025-10-31 19:41:49 +0000

    Basics mobile app

--- mobileApp/app/build.gradle.kts
+++ mobileApp/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.audiofingerprinting"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.audiofingerprinting"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -35,6 +35,8 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ mobileApp/app/src/main/java/com/example/audiofingerprinting/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.audiofingerprinting;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //get out button
+
+        TextView getTextView = (TextView) findViewById(R.id.textview1);
+        getTextView.setText(R.string.tap);
+    }
+}
