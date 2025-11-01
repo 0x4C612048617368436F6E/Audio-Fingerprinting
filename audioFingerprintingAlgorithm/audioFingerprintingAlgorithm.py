50c3fea8ae4f1c1ae18bc02907373b8b3826bb74
Benjamin <benjaminenadeba@gmail.com>
2025-11-01 18:27:29 +0000

    Add remaining

--- audioFingerprintingAlgorithm/audioFingerprintingAlgorithm.py
+++ audioFingerprintingAlgorithm/audioFingerprintingAlgorithm.py
@@ -5,4 +5,5 @@ class audioFingerPrintingAlgorithm:
     def __init__(self):
         pass
     def hash(self):
+        #simply pass in the peaks and hash
         pass
--- audioFingerprintingAlgorithm/audioFingerprintingMatching.py
+++ audioFingerprintingAlgorithm/audioFingerprintingMatching.py
@@ -4,4 +4,5 @@ class to facilitate audio finger printing matches
 
 class audioFingerPrintingMatching:
     def __init__(self):
+        #for matching stage
         pass
