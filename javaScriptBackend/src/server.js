31c5c804bdb6758dc22583965a3faaeb81a60dd9
Benjamin <benjaminenadeba@gmail.com>
2025-11-08 15:20:07 +0000

    Other stuff

--- javaScriptBackend/src/server.js
+++ javaScriptBackend/src/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//routes
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
