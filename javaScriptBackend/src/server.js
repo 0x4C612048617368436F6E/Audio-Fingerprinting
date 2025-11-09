6278032da63ce1eec37af4a379635b2407cd1132
Benjamin <benjaminenadeba@gmail.com>
2025-11-09 15:09:21 +0000

    changes to server

--- javaScriptBackend/src/server.js
+++ javaScriptBackend/src/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const addSongToDatabaseRouter = require("./api/router/addSongToDatabaseRouter");
+const audioFingerPrintRouter = require("./api/router/audioFingerPrintRouter");
 
+//configure form submit middleware
+app.use(express.urlencoded({ extended: true }));
+
+//configure json middleware
+app.use(express.json());
 //routes
+app.use("/api/addSongToDatabase", addSongToDatabaseRouter);
+app.use("/api/computeFingerPint", audioFingerPrintRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
